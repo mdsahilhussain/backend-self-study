8ec85230c5790459e47d7f826ca8d14cfba1d544
mdsahilhussain <mdsahil.hussain7@gmail.com>
2025-09-03 08:35:26 +0530

    utils code decides

--- src/index.js
+++ src/index.js
@@ -5,9 +5,24 @@ require("dotenv").config({ path: "./env" });
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error("Express app error:", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  });
 //!  "dev": "nodemon -r dotenv/config --experimental-json-modules src/index.js "
 
 /*
@@ -33,3 +48,4 @@ const app = express();
   }
 })();
 */
+ 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,39 @@
+/* //! note: this is a utility function to handle async functions in express routes
+ it takes a function as an argument and returns a function that calls the original function and catches any errors
+ and passes them to the next middleware
+
+ this is useful to avoid try catch blocks in every route handler
+ it is used like this:
+ app.get('/route', asyncHandler(async (req, res, next) => {
+    your code here
+ }));
+*/
+
+// const asyncHandler = () => {};
+// const asyncHandler = (fn) => {() => {}};
+// const asyncHandler = (fn) => async () => {};
+
+/* //! try catch block is used to catch errors in async functions
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(error.code || 500).json({
+      success: false,
+      message: error.message || "Internal Server Error",
+    });
+  }
+};
+*/
+
+//! better way is to use Promise.resolve and catch
+//! this way we don't have to use try catch block
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export default asyncHandler; 
